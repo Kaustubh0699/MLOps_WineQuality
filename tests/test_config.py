c8d778315dd0b66ad8ee135fe31aac817f3c0fee
Kaustubh Goyal <kaustubhgoyal118@gmail.com>
2022-05-16 17:42:22 +0530

    Modifies Tox File to download dvc

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,14 @@
+import pytest
 
 
+class NotInRange(Exception):
+    def __init__(self,message = "Value Not In Range"):
+        self.message = message
+        super().__init__(self.message)
 
 
 def test_generic():
-    a = 2
-    b = 2
-    assert a==b
+    a = 11
+    with pytest.raises(NotInRange):
+        if(a not in range(10,20)):
+            raise NotInRange
