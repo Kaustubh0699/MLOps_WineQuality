82db99dd233b84013cad25789c0b36950fb48efc
Kaustubh Goyal <kaustubhgoyal118@gmail.com>
2022-06-05 02:40:40 +0530

    Retrain Pipeline Test 1

--- add_run_details.py
+++ add_run_details.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 with open("runs.json",'r') as file:
     data = json.load(file)
-    data.append({'Run_name':"Trial",'Retrain_TBName':'XYZ',"Retrain_DateTime": datetime.now()})
+    date = datetime.now().strftime("%d-%m-%y")
+    data.append({'Run_name':"Trial",'Retrain_TBName':'XYZ',"Retrain_DateTime": str(date)})
 
 with open("runs.json",'w') as w_file:
     json.dump(data,w_file,indent=3)
